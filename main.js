c054cbbb089311d72ddd2e37963ca615fdd1acc3
Rehab-Alshaabi <125756697+Rehab-Alshaabi@users.noreply.github.com>
2023-02-18 20:37:31 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+//document.getElementById("count-el").innerHTML=0;
+//object () 
+
+var myOpject=new Object();
+myOpject["0"]="r";
+myOpject["1"]="e";
+myOpject["2"]="h";
+myOpject["3"]="a";
+myOpject["4"]="b";
+console.log(myOpject);
